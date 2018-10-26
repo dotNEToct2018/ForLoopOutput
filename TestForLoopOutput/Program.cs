4dabbfbe431e0c95049a08246d0254d900f2c940
DanE <edwardsdan001@gmail.com>
2018-10-26 14:52:00 -0400

    initial commit

---
+++ TestForLoopOutput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForLoopOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            for (int i = 20; i > 0; i -= 2)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+        }
+    }
+}
